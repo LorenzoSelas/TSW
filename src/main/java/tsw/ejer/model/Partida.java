11d1ade67ecbd737df284bb5bbd1c0e1eeaef44c
AdrianMG00 <Adrian.Martin6@alu.uclm.es>
2023-11-16 18:45:12 +0100

    Trabajando en el JSONArray para guardar cartas

--- src/main/java/tsw/ejer/model/Partida.java
+++ src/main/java/tsw/ejer/model/Partida.java
@@ -5,6 +5,9 @@ import java.util.List;
 import java.util.Random;
 import java.util.UUID;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import tsw.ejer.Excepcion.MovimientoIlegalException;
 import tsw.ejer.Excepcion.NotImplementedException;
 
@@ -12,12 +15,15 @@ import tsw.ejer.Excepcion.NotImplementedException;
 
 //@Data
 public class Partida {
+    
     private String id;
     private boolean sentido;
     private Carta ultimaCarta;
     private User jugadorConTurno;
     private List<User> users;
-
+    private JSONArray cartas;
+    private int numeroCarta;
+    private Random random;
     public Partida(){
         id = UUID.randomUUID().toString();
         this.users = new ArrayList<>();
@@ -46,7 +52,7 @@ public class Partida {
             case 'n':
                 if (carta.getColor() == this.ultimaCarta.getColor() || carta.getNumero() == this.ultimaCarta.getNumero()){
                     this.ultimaCarta = carta;
-                    pasarTurno();
+                    pasarTurno(carta);
                 } else {
                     throw new MovimientoIlegalException("Esta carta no se puede jugar");
                 }
@@ -54,8 +60,8 @@ public class Partida {
             case 'b':
                 if (carta.getColor() == this.ultimaCarta.getColor() || carta.getTipo() == this.ultimaCarta.getTipo()){
                     this.ultimaCarta = carta;
-                    pasarTurno();
-                    pasarTurno();
+                    pasarTurno(carta);
+                    pasarTurno(carta);
                 } else {
                     throw new MovimientoIlegalException("Esta carta no se puede jugar");
                 }
@@ -64,13 +70,19 @@ public class Partida {
                 if (carta.getColor() == this.ultimaCarta.getColor() || carta.getTipo() == this.ultimaCarta.getTipo()){
                     this.ultimaCarta = carta;
                     sentido = !this.sentido;
-                    pasarTurno();
+                    pasarTurno(carta);
                 } else {
                     throw new MovimientoIlegalException("Esta carta no se puede jugar");
                 }
                 break;
             case 's':
-                throw new NotImplementedException("Esta funcionalidad aun no esta implementada");
+                if(carta.getTipo() == this.ultimaCarta.getTipo() && carta.getCantidad() == this.ultimaCarta.getCantidad()){
+                    this.ultimaCarta =carta;
+                    pasarTurno(carta);
+                }else{
+                    throw new NotImplementedException("Esta carta no se puede jugar");
+                }
+                
             case 'c':
                 throw new NotImplementedException("Esta funcionalidad aun no esta implementada");
             default:
@@ -78,7 +90,7 @@ public class Partida {
         }
     }
 
-    private void pasarTurno() {
+    private void pasarTurno(Carta carta) {
         int actual = users.indexOf(this.jugadorConTurno);
         if (sentido)
             if (actual + 1 == this.users.size())
@@ -90,6 +102,9 @@ public class Partida {
                 this.jugadorConTurno = this.users.get(this.users.size() - 1);
             else
                 this.jugadorConTurno = this.users.get(actual - 1);
+        if(carta.getCantidad()== 2){
+            cartas.put(new Random().nextInt(0-9), carta);
+        }
     }
 
     public List<User> getUsers(){
@@ -106,8 +121,35 @@ public class Partida {
     public void iniciar(){
         this.jugadorConTurno = this.users.get(new Random().nextInt(this.users.size()));
         this.sentido = true;
-        /*
-         * repartir cartas de un posible json
-         */
+        //Ideas para generar cartas y guardarlas en un JSONArray
+        for(int i =0; i<80;i++ ){
+            //cartas.put(crearCarta(ultimaCarta.getNumero(generarNumero()), ultimaCarta.getColor(), ultimaCarta.getCantidad()));
+        }
+    }
+
+    private int generarNumero(){
+        int numero= random.nextInt(6);
+        //numero = 0, carta numérica
+        if(numero ==0){
+            numero= random.nextInt(10);
+        } else{
+            numero= '\0';
+        }
+        return numero;
+    }
+
+    private static JSONArray crearCarta(int numero, char color, int tipo) {
+        JSONArray carta = new JSONArray();
+        carta.put(crearAtributo("Numero de carta", numero));
+        carta.put(crearAtributo("Color", color));
+        carta.put(crearAtributo("Tipo de carta", tipo));
+        return carta;
+    }
+
+    // Método para crear un objeto JSON que representa un atributo de la carta
+    private static JSONObject crearAtributo(String nombre, Object valor) {
+        JSONObject atributo = new JSONObject();
+        atributo.put(nombre, valor);
+        return atributo;
     }
 }
